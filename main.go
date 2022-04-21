c7317e6d712d81d805ef2b629900d5539cd2da2a
Chung <chungnd1@smartosc.com>
2022-04-21 14:10:40 +0700

    feat: list pages

--- main.go
+++ main.go
@@ -5,18 +5,35 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gomarkdown/markdown"
 	"html/template"
+	"io/fs"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	router := gin.Default()
-
 	router.LoadHTMLGlob("templates/*")
+	dataDir := "data/pages"
+
+	router.GET("/", func(c *gin.Context) {
+		pages := []string{}
+		filepath.WalkDir(dataDir, func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				return nil
+			}
+			pages = append(pages, path[len(dataDir):])
+			return nil
+		})
+
+		c.HTML(http.StatusOK, "list.html", gin.H{
+			"pages": pages,
+		})
+	})
 
 	router.GET("/wiki/*page", func(c *gin.Context) {
 		page := c.Param("page")
-		file := "./data/pages" + page
+		file := dataDir + page
 		wikiContent, err := os.ReadFile(file)
 
 		if err != nil {
