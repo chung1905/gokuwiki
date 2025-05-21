381e1706a507f9e1d6ab3bca31e8b8fe307c0719
chung <me@chungn.com>
2025-05-21 16:37:36 +0000

    add async loading for wiki.js script in the header

---
+++ web/pub/js/wiki.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', function () {
+    requestAnimationFrame(function () {
+        const codeBlocks = document.querySelectorAll('pre code, code');
+
+        codeBlocks.forEach(function (codeBlock) {
+            const container = codeBlock.parentNode.tagName === 'pre' ?
+                codeBlock.parentNode :
+                codeBlock;
+
+            container.style.position = 'relative';
+
+            const copyButton = document.createElement('button');
+            copyButton.innerText = 'Copy';
+            copyButton.className = 'copy-button';
+            copyButton.style.cssText = 'position: absolute; top: 5px; right: 5px; font-size: small; padding: 2px 5px; z-index: 10; opacity: 0.7;';
+
+            copyButton.addEventListener('mouseover', function () {
+                this.style.opacity = '1';
+            });
+
+            copyButton.addEventListener('mouseout', function () {
+                this.style.opacity = '0.7';
+            });
+
+            copyButton.addEventListener('click', function () {
+                const textToCopy = codeBlock.textContent;
+
+                navigator.clipboard.writeText(textToCopy)
+                    .then(() => {
+                        const originalText = copyButton.innerText;
+                        copyButton.innerText = 'Copied!';
+
+                        setTimeout(() => {
+                            copyButton.innerText = originalText;
+                        }, 2000);
+                    })
+                    .catch(err => {
+                        console.error('Failed to copy text: ', err);
+                    });
+            });
+
+            container.appendChild(copyButton);
+        });
+    });
+});
